f20c91598d009759dbee3a34506d0b3f3e4c1a38
wangtao <wangtao.steven@bytedance.com>
2024-06-17 18:47:40 +0800

    init APMPlus Harmony Demo

---
+++ entry/src/main/cpp/main_app.cpp
@@ -0,0 +1,46 @@
+#include "napi/native_api.h"
+
+// NativeCrash test
+static void test_crash() {
+    volatile int *a = (int *)(0);
+    *a = 1;
+}
+
+static napi_value DoCrash(napi_env env, napi_callback_info info)
+{
+    size_t requireArgc = 2;
+    size_t argc = 2;
+    napi_value args[2] = {nullptr};
+
+    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
+
+    test_crash();
+    
+    return 0;
+}
+
+EXTERN_C_START
+static napi_value Init(napi_env env, napi_value exports)
+{
+    napi_property_descriptor desc[] = {
+        { "doCrash", nullptr, DoCrash, nullptr, nullptr, nullptr, napi_default, nullptr }
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    return exports;
+}
+EXTERN_C_END
+
+static napi_module demoModule = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Init,
+    .nm_modname = "native_lib",
+    .nm_priv = ((void*)0),
+    .reserved = { 0 },
+};
+
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
+{
+    napi_module_register(&demoModule);
+}
